3dd0ca8342c1dbf3fdc11a40b51bdf37de2b4c00
root <pc_89@hotmail.es>
2020-03-18 13:11:13 +0000

    archivos borrados

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -4,36 +4,13 @@ const api = axios.create({
   timeout: 1000
 })
 
-document.getElementById('btn-signup').addEventListener('click', (event) => {
-  const newUser = {
-    user_name: document.getElementById('user_name').value,
-    user_email: document.getElementById('user_email').value,
-    user_password: document.getElementById('user_password').value
-  }
-
-  api
-    .post('auth/signup', newUser)
-    //
-    // Aqui es donde el backend crea el usuario
-    // crea el token y me lo devuelve
-    //
-    .then(function (response) {
-      localStorage.setItem('token', response.data.token)
-      localStorage.setItem('name', response.data.username)
-      localStorage.setItem('email', response.data.email)
-      location.assign('clubs.html')
-    })
-    .catch(function (error) {
-      console.log(error.response)
-    })
-})
-
 document.getElementById('btn-login').addEventListener('click', (event) => {
+  console.log('click')
   const newUser = {
     user_email: document.getElementById('login_email').value,
     user_password: document.getElementById('login_password').value
   }
-
+  console.log('info')
   api
     .post('auth/login', newUser)
     .then(function (response) {
