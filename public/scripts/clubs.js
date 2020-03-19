f61a7dd202a2dfbe1794fbfda6cda06f8967712b
root <pc_89@hotmail.es>
2020-03-19 18:39:44 +0000

    clubs html

--- public/scripts/clubs.js
+++ public/scripts/clubs.js
@@ -1,6 +1,6 @@
 
 function changeColor (x) {
-  if (x.style.background == '#696969') {
+  if (x.style.background === '#696969') {
     x.style.background = 'rgb(255,0,0)'
   } else {
     x.style.background = 'rgb(255,0,0)'
