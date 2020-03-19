deae88f6fd8d45be0a65166b2c72b864176eb603
root <pc_89@hotmail.es>
2020-03-19 18:35:03 +0000

    html clubs

---
+++ public/scripts/clubs.js
@@ -0,0 +1,9 @@
+
+function changeColor (x) {
+  if (x.style.background == '#696969') {
+    x.style.background = 'rgb(255,0,0)'
+  } else {
+    x.style.background = 'rgb(255,0,0)'
+  }
+  return false
+}
